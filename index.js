c2e86ebd315820ad611a4f5462ad5a45408384b5
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-08-14 20:44:46 +0630

    bug fix

--- index.js
+++ index.js
@@ -4,31 +4,29 @@ const yauzl = require('yauzl');
 
 function unzip(filePath, fileName) {
   return new Promise((resolve, reject) => {
-    yauzl.open(filePath, { lazyEntries: true }, (err, zipfile) => {
-      if (err) {
-        reject(err);
-      } else {
+    try {
+      yauzl.open(filePath, { lazyEntries: true }, (err, zipfile) => {
+        if (err) throw err;
         zipfile.readEntry();
         zipfile.on('entry', entry => {
           if (entry.fileName === fileName) {
             zipfile.openReadStream(entry, (err, stream) => {
-              if (err) {
-                reject(err);
-              } else {
-                const buf = [];
-                stream.on('data', chunk => {
-                  buf.push(chunk);
-                });
-                stream.on('end', () => {
-                  resolve(Buffer.concat(buf));
-                });
-              }
+              if (err) throw err;
+              const buf = [];
+              stream.on('data', chunk => {
+                buf.push(chunk);
+              });
+              stream.on('end', () => {
+                resolve(Buffer.concat(buf));
+              });
             });
           }
           zipfile.readEntry();
         });
-      }
-    });
+      });
+    } catch (error) {
+      reject(error);
+    }
   });
 }
 
